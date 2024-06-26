f48aee283f3e74d1b525c24fea2ef742b6a1c71d
Ccode25 <ajdaelo25@gmail.com>
2024-06-26 21:15:10 +0800

    added touch event

--- game.js
+++ game.js
@@ -3,6 +3,7 @@ let gamePattern = [];
 let start = false;
 let gameLevel = 1;
 let userClickedPattern = [];
+let detectTap = false;
 
 $(document).on('touchstart', function() {
   detectTap = true; // Detects all touch events
@@ -11,59 +12,53 @@ $(document).on('touchmove', function() {
   detectTap = false; // Excludes the scroll events from touch events
 });
 $(document).on('click touchend', function(event) {
-  if (event.type == "click") detectTap = true; // Detects click events
-     if (detectTap){
-      $(document).keydown(function() {
-        if (!start) {
-          start = true;
-          headerOutput()
-          nextSequence();
-          clickColor();
-        }
-      });
-      
-     }
-});
-
-$(document).keydown(function() {
-  if (!start) {
-    start = true;
-    headerOutput()
-    nextSequence();
-    clickColor();
+  if (event.type === "click") detectTap = true; // Detects click events
+  if (detectTap) {
+    startGame();
   }
 });
 
+$(document).keydown(function() {
+  startGame();
+});
+
+function startGame() {
+  if (!start) {
+    start = true;
+    headerOutput();
+    nextSequence();
+    clickColor();
+  }
+}
 
 function clickColor() {
   $("#red").on("click", randomChosenColour);
   $("#blue").on("click", randomChosenColour);
   $("#green").on("click", randomChosenColour);
   $("#yellow").on("click", randomChosenColour);
-  }
+}
 
-function unclickColor () {
+function unclickColor() {
   $("#red").off("click", randomChosenColour);
   $("#blue").off("click", randomChosenColour);
   $("#green").off("click", randomChosenColour);
   $("#yellow").off("click", randomChosenColour);
 }
 
-  // Function to update the header of the game
-function headerOutput () {
-    $("h1").text("Level " + gameLevel);
-  }
+// Function to update the header of the game
+function headerOutput() {
+  $("h1").text("Level " + gameLevel);
+}
 
 // Function to generate next sequence color
-function nextSequence () {
+function nextSequence() {
   let randomNumbers = Math.floor(Math.random() * 4);
   let randomColour = buttonColour[randomNumbers];
   gamePattern.push(randomColour);
-  playSound(randomColour); 
+  playSound(randomColour);
   fadeEffect($("#" + randomColour));
-  userClickedPattern = [] //reset user pattern for new sequence 
+  userClickedPattern = []; //reset user pattern for new sequence 
 }
-
 
 // Function to handle button click and user input
 function randomChosenColour() {
@@ -78,9 +73,7 @@ function randomChosenColour() {
     playSound("wrong");
     unclickColor();
     startOver();
-  }
-
-  else if (gamePattern.length === userClickedPattern.length) {
+  } else if (gamePattern.length === userClickedPattern.length) {
     setTimeout(function() {
       gameLevel++;
       headerOutput();
@@ -99,59 +92,9 @@ function checkClickPattern() {
   return true;
 }
 
-
-
 function fadeEffect(button) {
   button.animate({ opacity: 0 }, 20).animate({ opacity: 1 }, 100);
-
 }
 
 function playSound(name) {
-  let sound = new Audio("./sounds/" + name + ".mp3");
-  sound.play();
-}
-
-function startOver() {
-  gamePattern = [];
-  userClickedPattern = [];
-  gameLevel = 1;
-  start = false;
-}
-
-
-// function playSound(name) {
-
-//   switch (name) {
-//     case "blue":
-//       let blueSound = new Audio("./sounds/blue.mp3");
-//       blueSound.play();
-//     break;
-
-//     case "green":
-//       let greenSound = new Audio("./sounds/green.mp3");
-//       greenSound.play();
-//     break;
-
-//     case "red":
-//       let redSound = new Audio("./sounds/red.mp3");
-//       redSound.play();
-//     break;
-
-//     case "yellow":
-//       let yellowSound = new Audio("./sounds/yellow.mp3");
-//       yellowSound.play();
-//     break;
-
-//     default:
-//       let wrongSound = new Audio("./sounds/wrong.mp3");
-//       wrongSound.play();
-//     break;
-
-
-//   }
-
-// }
-
-
-
-
+  let sound = new Audio("./sounds/"
